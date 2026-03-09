b2418ae46096e3ada1a889f1a6dbd889f1fd54c2
Pomuch40 <Pomuch40@DESKTOP-GP71DTO>
2026-03-09 08:19:57 -0600

    fix: añadida calculador basica

---
+++ src/practicagit_avm/PracticaGit_avm.java
@@ -0,0 +1,42 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package practicagit_avm;
+
+/**
+ *
+ * @author Pomuch40
+ */
+public class PracticaGit_avm {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("---sistema de Gestion de alumnos---");
+        System.out.println("Bienvenido, Usuario Estudiante");
+        
+  
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    }
+    
+}
