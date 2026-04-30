ed3d3d5e0f29f683b43b55e4a45a44f7e9f4a372
ALESSANDRA VIDAL MENDEZ <alessandra.vidal_pm25238@cecyte.edu.mx>
2026-04-30 11:44:31 -0600

    Add files via upload

---
+++ Edad.java
@@ -0,0 +1,53 @@
+
+import java.util.Scanner;
+
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+
+/**
+ *
+ * @author Pomuch40
+ */
+public class Edad {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+     
+        Scanner Scanner = new Scanner (System.in);
+        
+        for(int n =1; n < = 10;n++);
+        
+        System.out.print ("Teclee el año actual: ");
+        
+        int Año_act = Scanner.nextInt();
+        
+        System.out.print ("Teclee el año de nacimiento");
+        
+        int Año_nac = Scanner.nextInt();
+       
+        
+        int edad = Año_act - Año_nac;
+                
+                System.out.println("La edad es:" + edad);
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+    }
+    
+}
