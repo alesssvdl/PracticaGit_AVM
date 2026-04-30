96a7d4c556f5b1daf7f60c6a7e4992b447277219
ALESSANDRA VIDAL MENDEZ <alessandra.vidal_pm25238@cecyte.edu.mx>
2026-04-30 11:40:44 -0600

    Add files via upload

---
+++ Area.java
@@ -0,0 +1,54 @@
+
+import java.util.Scanner;
+
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+
+/**
+ *
+ * @author Pomuch40
+ */
+public class Area {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+      
+        Scanner scanner = new Scanner(System.in);
+   
+    for (int n1.n< 10 n++);
+  
+    System.out.print("Teclee un numero: ");
+   
+    int base= scanner.nextInt();
+    
+    
+   
+    int altura = base*altura
+           
+            System.out.println("La base y altura es: area");
+
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+    }
+    
+}
